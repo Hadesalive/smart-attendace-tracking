3baaf97bab2b0ea309a80e11d024479e9ff5de0a
isata morohm bah <isatamorombahicloud.com@Istas-MacBook-Pro.local>
2025-07-15 18:21:56 +0000

    feat: add env var checks for debugging

--- lib/supabase/admin.ts
+++ lib/supabase/admin.ts
@@ -4,8 +4,16 @@ import { createClient } from "@supabase/supabase-js"
 // such as creating users, bypassing RLS, etc. It uses the SERVICE_ROLE_KEY.
 // IMPORTANT: This client should NEVER be used on the client-side.
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const serviceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY!
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+const serviceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY
+
+if (!supabaseUrl) {
+  throw new Error("Missing env.NEXT_PUBLIC_SUPABASE_URL")
+}
+
+if (!serviceRoleKey) {
+  throw new Error("Missing env.SUPABASE_SERVICE_ROLE_KEY")
+}
 
 export const supabaseAdmin = createClient(supabaseUrl, serviceRoleKey, {
   auth: {
